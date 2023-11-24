66d7ed44df20f633db0502899c056fd38bc029ac
EmmanuelHdez <84370797+EmmanuelHdez@users.noreply.github.com>
2023-11-23 18:15:51 -0600

    first

---
+++ android/app/src/main/kotlin/com/applicationbase/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.applicationbase.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/core/utils/image_constant.dart
@@ -0,0 +1,60 @@
+class ImageConstant {
+  static String imgHeader = 'assets/images/psychiatry-uk-logo.png';
+  static String imgShape50x502 = 'assets/images/img_shape_50x50_2.png';
+
+  static String imgLocation9x9 = 'assets/images/img_location_9x9.svg';
+
+  static String imgText13 = 'assets/images/img_text_13.png';
+
+  static String imgMenu1 = 'assets/images/img_menu_1.svg';
+
+  static String imgShape50x501 = 'assets/images/img_shape_50x50_1.png';
+
+  static String imgText14 = 'assets/images/img_text_14.png';
+
+  static String imgMenu = 'assets/images/img_menu.svg';
+
+  static String imgShape50x504 = 'assets/images/img_shape_50x50_4.png';
+
+  static String imgAe45615de12342ab99f19311ea988aa7 =
+      'assets/images/img_ae45615de12342ab99f19311ea988aa7.svg';
+
+  static String imgShape50x601 = 'assets/images/img_shape_50x60_1.png';
+
+  static String imgText15 = 'assets/images/img_text_15.png';
+
+  static String imgButtoncategory1 = 'assets/images/img_buttoncategory_1.png';
+
+  static String imgButtoncategory = 'assets/images/img_buttoncategory.png';
+
+  static String imgShape50x503 = 'assets/images/img_shape_50x50_3.png';
+
+  static String imgShape140x1682 = 'assets/images/img_shape_140x168_2.png';
+
+  static String imgButtoncategory25x25 =
+      'assets/images/img_buttoncategory_25x25.png';
+
+  static String imgLocationDeepOrangeA200 =
+      'assets/images/img_location_deep_orange_a200.svg';
+
+  static String imgShape50x602 = 'assets/images/img_shape_50x60_2.png';
+
+  static String imgStar9x9 = 'assets/images/img_star_9x9.svg';
+
+  static String imgVectorGray100 = 'assets/images/img_vector_gray_100.svg';
+
+  static String imgSplashscreen = 'assets/images/img_splashscreen.png';
+
+  static String imgFavoriteWhiteA700 =
+      'assets/images/img_favorite_white_a700.svg';
+
+  static String imgShape140x1261 = 'assets/images/img_shape_140x126_1.png';
+
+  static String imgStar1 = 'assets/images/img_star_1.svg';
+
+  static String imgText12 = 'assets/images/img_text_12.png';
+
+  static String imgShape140x1683 = 'assets/images/img_shape_140x168_3.png';
+
+  static String imageNotFound = 'assets/images/image_not_found.png';
+}
